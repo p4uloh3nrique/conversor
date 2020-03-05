21611f2fedc5c2fd62524bef94879dcba1b80eac
Your Name <you@example.com>
2020-03-05 10:15:40 -0300

    Inclusão de comentários (descrição e autor).

--- convert.sh
+++ convert.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Este script tem a função de converter uma imagem com extensão JPG em uma imagem com extensão PNG.
+#
+# Autor: Paulo Henrique
+#
+
 cd /home/aluno/trabalhon1/imagens-livros
 for imagem in *.jpg
 do
